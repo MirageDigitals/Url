bf97847c436d806ef331e13c7efe18ae30eeb307
sinakuhestani <admin@kuhestani.org>
2024-04-02 06:33:26 +0330

    add fragment trait

---
+++ src/Partials/FragmentTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Mirage Framework (https://miragedigitals.org/framework)
+ * Copyright (c) 2024 Sina Kuhestani (https://kuhestani.org)
+ */
+
+declare(strict_types= 1);
+
+namespace Mirage\Url\Partials;
+
+trait FragmentTrait
+{
+    private ?string $fragment = NULL;
+
+    public function getFragment(): ?string
+    {
+        return $this->fragment;
+    }
+    
+    public function setFragment(?string $fragment): void
+    {
+        $this->fragment = $fragment;
+    }
+
+    public function setFragmentSafe(?string $fragment): void
+    {
+        if (TRUE === empty($fragment)) $fragment = NULL;
+        $this->setFragment($fragment);
+    }
+}
