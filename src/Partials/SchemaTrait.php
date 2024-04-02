aa6a48a07b6af0cc75c225a9ab28f69d167e8e2e
sinakuhestani <admin@kuhestani.org>
2024-04-02 06:31:29 +0330

    add scheme trait

---
+++ src/Partials/SchemaTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Mirage Framework (https://miragedigitals.org/framework)
+ * Copyright (c) 2024 Sina Kuhestani (https://kuhestani.org)
+ */
+
+declare(strict_types= 1);
+
+namespace Mirage\Url\Partials;
+
+trait SchemaTrait
+{
+    private ?string $scheme = NULL;
+
+    private string $deafultScheme = "https";
+
+    public function getScheme(): ?string
+    {
+        return $this->scheme;
+    }
+
+    public function setScheme(?string $scheme = NULL): void
+    {
+        $this->scheme = $scheme;
+    }
+
+    public function setSchemeSafe(?string $scheme = NULL): void
+    {
+        if (TRUE === empty($scheme)) $scheme = NULL;
+        $this->setScheme($scheme);
+    }
+}
