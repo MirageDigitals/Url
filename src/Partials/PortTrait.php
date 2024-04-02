d5b2d81f9cde7c32ebbfee2958598d2cb7db914d
sinakuhestani <admin@kuhestani.org>
2024-04-02 06:28:58 +0330

    add port trait

---
+++ src/Partials/PortTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Mirage Framework (https://miragedigitals.org/framework)
+ * Copyright (c) 2024 Sina Kuhestani (https://kuhestani.org)
+ */
+
+declare(strict_types= 1);
+
+namespace Mirage\Url\Partials;
+
+trait PortTrait
+{
+    private ?int $port = NULL;
+
+    private int $defaultPort = 80;
+
+    public function getPort(): ?int
+    {
+        return $this->port;
+    }
+
+    public function setPort(?int $port = NULL): void
+    {
+        $this->port = $port;
+    }
+
+    public function setPortSafe(?int $port = NULL): void
+    {
+        if (TRUE === empty($port)) $port = NULL;
+        $this->setPort($port);
+    }
+}
